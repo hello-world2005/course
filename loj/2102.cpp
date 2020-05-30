e5b7d62f1a475e945f3c6eb671572f029dcfbd4f
hello-world2005 <65162491+hello-world2005@users.noreply.github.com>
2020-05-30 23:10:33 +0800

    2020.5.30

--- loj/2102.cpp
+++ loj/2102.cpp
@@ -33,9 +33,9 @@ struct SuffixAutoMaton {
         int nq = ++tot;
         a[nq] = a[q];
         a[nq].len = a[p].len + 1;
-        a[nq].sze=0;
+        a[nq].sze = 0;
         a[q].fa = a[np].fa = nq;
-        for (;a[p].ch[c] == q; p = a[p].fa)
+        for (; a[p].ch[c] == q; p = a[p].fa)
           a[p].ch[c] = nq;
       }
     }
@@ -47,26 +47,57 @@ struct SuffixAutoMaton {
     for (int i = 1; i <= n; ++i)
       Insert(s[i] - 'a');
   }
-} sa;
+} sam;
 
 int n;
 char s[N << 1];
-int c[N << 1], d[N << 1];
+int c[N << 1], d[N << 1], v[N << 1], sum[N << 1];
 
-void Solve(int t, int k) {
-  sa.Build(s);
-  for (int i = 1; i <= sa.tot; ++i)
-    ++c[sa.a[i].len];
-  for (int i = 1; i <= sa.tot; ++i)
+void TopSort(int t) {
+  for (int i = 1; i <= sam.tot; ++i)
+    ++c[sam.a[i].len];
+  for (int i = 1; i <= sam.tot; ++i)
     c[i] += c[i - 1];
-  for (int i = sa.tot; i >= 1; --i)
-    d[c[sa.a[i].len]--] = i;
+  for (int i = sam.tot; i; --i)
+    d[c[sam.a[i].len]--] = i;
+  for (int i = 1; i <= sam.tot; ++i)
+    v[i] = sam.a[i].sze;
+  for (int i = sam.tot; i; --i)
+    if (t)
+      v[sam.a[d[i]].fa] += v[d[i]];
+    else
+      v[d[i]] = 1;
+  v[1] = 0;
+  for (int i = sam.tot; i; --i) {
+    sum[d[i]] = v[d[i]];
+    for (int j = 0; j < 26; ++j)
+      sum[d[i]] += sum[sam.a[d[i]].ch[j]];
+  }
+}
+
+void Print(int u, int k) {
+  if (k > v[u]) {
+    k -= v[u];
+    for (int i = 0; i < 26; ++i)
+      if (k <= sum[sam.a[u].ch[i]]) {
+        printf("%c", i + 'a');
+        Print(sam.a[u].ch[i], k);
+        return;
+      } else {
+        k -= sum[sam.a[u].ch[i]];
+      }
+  }
 }
 
 int main() {
   scanf("%s", s + 1);
+  sam.Build(s);
   int typ, k;
   scanf("%d%d", &typ, &k);
-  Solve(typ, k);
+  TopSort(typ);
+  if (k > sum[1])
+    printf("-1\n");
+  else
+    Print(1, k);
   return 0;
 }
