80ff86cf7d896631d4ad3f131f4d2e9f0e69b8f1
hello-world2005 <65162491+hello-world2005@users.noreply.github.com>
2020-07-13 08:04:17 +0800

    2020.7.12

--- codeforces/1380/e.cpp
+++ codeforces/1380/e.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 #include <set>
 
-const int N = 1e5 + 10;
+const int N = 2e5 + 10;
 
 int n, m;
 int a[N];
@@ -17,28 +17,35 @@ int FindFa(int u) {
 
 int main() {
   scanf("%d%d", &n, &m);
-  for (int i = 1; i <= n; ++i) {
+  m -= 1;
+  for (int i = 0; i < n; ++i) {
     scanf("%d", &a[i]);
+    a[i] -= 1;
     s[a[i]].insert(i);
     fa[i] = i;
   }
   int ans = 0;
-  for (int i = 1; i < n; ++i)
+  for (int i = 0; i < n - 1; ++i)
     if (a[i] != a[i + 1])
       ++ans;
-  for (int i = 1; i <= m; ++i) {
+  printf("%d\n", ans);
+  while (m--) {
     int x, y;
     scanf("%d%d", &x, &y);
+    x -= 1, y -= 1;
     int fx = FindFa(x), fy = FindFa(y);
     if (s[fx].size() > s[fy].size())
       std::swap(fx, fy);
     for (auto i : s[fx]) {
-      auto it = s[y].lower_bound(i);
+      auto it = s[fy].lower_bound(i);
       if (it != s[fy].end())
-        ans += (*it == (i + 1));
+        ans -= ((*it) == i + 1);
       if (it != s[fy].begin())
-        it--, ans -= (i == (*it - i));
+        it--, ans -= (i == (*it) + 1);
     }
+    for (auto i : s[fx])
+      s[fy].insert(i);
+    fa[fx] = fy;
     printf("%d\n", ans);
   }
   return 0;
