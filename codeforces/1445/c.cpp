1ad10423f5df24ba8dbe64e93a6dcad4eb41c18f
hello-world2005 <65162491+hello-world2005@users.noreply.github.com>
2020-11-02 19:44:03 +0800

    2020.11.2

--- codeforces/1445/c.cpp
+++ codeforces/1445/c.cpp
@@ -1,81 +1,30 @@
+#include <algorithm>
 #include <cstdio>
-#include <vector>
 
 #define int long long
-
-int Pow(int a, int b) {
-  int res = 1;
-  for (int i = 1; i <= b; ++i)
-    res *= b;
-  return res;
-}
 
 signed main() {
   int T;
   scanf("%lld", &T);
   while (T--) {
-    int p, q, x;
+    int p, q, ans = 0;
     scanf("%lld%lld", &p, &q);
-    if (p % q != 0) {
-      printf("%lld\n", p);
-    } else {
-      std::vector<std::pair<int, int> > v1, v2;
-      int t = p / q;
-      for (int i = 2; i * i <= t; ++i)
-        if (t % i == 0) {
-          int _ = t, cnt = 0;
-          while (_ % i == 0)
-            _ /= i, ++cnt;
-          v1.push_back(std::make_pair(i, cnt));
-          if (i * i != t) {
-            int _ = t, cnt = 0, j = t / i;
-            while (_ % j == 0)
-              _ /= j, ++cnt;
-            v1.push_back(std::make_pair(j, cnt));
-          }
-        }
-      for (int i = 2; i * i <= q; ++i)
-        if (q % i == 0) {
-          int _ = q, cnt = 0;
-          while (_ % i == 0)
-            _ /= i, ++cnt;
-          v2.push_back(std::make_pair(i, cnt));
-          if (i * i != t) {
-            int _ = t, cnt = 0, j = q / i;
-            while (_ % j == 0)
-              _ /= j, ++cnt;
-            v2.push_back(std::make_pair(j, cnt));
-          }
-        }
-      // printf("%lld\n", (int)v2.size());
-      if (v2.size() == 0) {
-        int ans = p;
-        while (ans % q == 0)
-          ans /= q;
-        printf("%lld\n", ans);
-        continue;
+    for (int i = 2; i * i <= q; ++i)
+      if (q % i == 0) {
+        int now = 1, cnt = 0;
+        while (q % i == 0)
+          q /= i, ++cnt, now *= i;
+        int _ = p;
+        while (_ % now == 0)
+          _ /= i;
+        ans = std::max(ans, _);
       }
-      long long ans = 0;
-      for (int i = 0; i < (int)v2.size(); ++i) {
-        int now = 1;
-        int x = v2[i].first;
-        printf(">> %lld\n", v2[i].first);
-        now = q / x;
-        for (int j = 0; j < (int)v1.size(); ++j)
-          if (v1[j].first != x) {
-            printf("%lld\n", v1[j].first);
-            now *= Pow(v1[j].first, v1[j].second);
-          }
-        ans = std::max(ans, now);
-      }
-      // for (int i = 1; i < (int)v1.size(); ++i)
-      //   if (v1[i] != x)
-      //     ans *= v1[i];
-      // for (int i = 0; i < (int)v2.size(); ++i)
-      //   if (v1[i].first != x)
-      //     ans *= Pow(v1[i].first, v1[i].second);
-      printf("%lld\n", ans);
+    if (q > 1) {
+      int _ = p;
+      while (_ % q == 0)
+        _ /= q;
+      ans = std::max(ans, _);
     }
+    printf("%lld\n", ans);
   }
-  return 0;
 }
--- codeforces/1445/d.cpp
+++ codeforces/1445/d.cpp
@@ -1,7 +1,42 @@
+#include <algorithm>
 #include <cstdio>
 
-int main() {
-  int T;
-  scanf("%d", &T);
-  while (T--) {}
+#define int long long
+
+const int N = 3e5 + 10;
+const int MOD = 998244353;
+
+int a[N];
+int sum[2];
+
+int FastPow(int a, int b) {
+  int res = 1;
+  for (; b; b >>= 1, a = a * a % MOD)
+    if (b & 1)
+      res = res * a % MOD;
+  return res;
+}
+
+int fac[N], inv[N];
+
+void Init(const int& n) {
+  fac[0] = 1;
+  for (int i = 1; i <= n; ++i)
+    fac[i] = fac[i - 1] * i % MOD, inv[i] = FastPow(fac[i], MOD - 2);
+}
+
+int C(int n, int m) {
+  return fac[n] * inv[n - m] % MOD * inv[m] % MOD;
+}
+
+signed main() {
+  int n;
+  scanf("%lld", &n);
+  Init(n << 1);
+  for (int i = 1; i <= 2 * n; ++i)
+    scanf("%lld", &a[i]);
+  std::sort(a + 1, a + 2 * n + 1);
+  for (int i = 1; i <= 2 * n; ++i)
+    (sum[i > n] += a[i]) %= MOD;
+  printf("%lld\n", (sum[1] - sum[0] + MOD) % MOD * C(2 * n, n) % MOD);
 }
