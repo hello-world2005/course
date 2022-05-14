28d20903d43ea092e1295c4f68dc4bd594c08fdd
hello-world2005 <hello_world2005@outlook.com>
2022-05-14 22:10:21 +0800

    update

---
+++ 学校/python入门/pandas/test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel('2.xlsx', sheet_name='原始')
+print(df)
--- 学校/信息技术/出题/2/chk.cpp
+++ 学校/信息技术/出题/2/chk.cpp
@@ -4,15 +4,15 @@
 int main() {
   for (int i = 1; i <= 10; ++i) {
     char com[200];
-    sprintf(com, "python3 poker.py < \"Problem Poker/data/poker%d.in\" > \"Problem Poker/data/poker%d.out\"", i, i);
+    sprintf(com, "time python3 poker.py < \"Texas Hold'em Poker/data/poker%d.in\" > example.out", i);
     system(com);
     // sprintf(com, "./poker < \"Problem Poker/data/poker%d.in\" > \"Problem Poker/data/poker%d.ans\"", i, i);
     // system(com);
     printf("==========%d==========\n", i);
-    sprintf(com, "diff \"Problem Poker/data/poker%d.ans\" \"Problem Poker/data/poker%d.out\"", i, i);
-    system(com);
-    sprintf(com, "rm \"Problem Poker/data/poker%d.out\"", i);
-    system(com);
+    // sprintf(com, "diff \"Problem Poker/data/poker%d.ans\" \"Problem Poker/data/poker%d.out\"", i, i);
+    // system(com);
+    // sprintf(com, "rm \"Problem Poker/data/poker%d.out\"", i);
+    // system(com);
   }
   return 0;
 }
